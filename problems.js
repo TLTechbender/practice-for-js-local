8e36bb0ae8e1087a9bbf87da6920cf6d07fa4d68
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-07-29 16:01:46 +0100

    done

--- problems.js
+++ problems.js
@@ -1,23 +1,35 @@
 function divideByThree(num) {
     /* Returns the passed in number argument divided by three. */
     // Your code here
+let answer= num/3;
+    return answer;
 };
 
 function averageOfTwo(num1, num2) {
     /* Returns the average of two numbers, num1 and num2. */
     // Your code here
+let sum= num1 + num2;
+    return sum/2;
 };
 
 function averageOfFour(num1, num2, num3, num4) {
     /* Takes in four numbers. The function should return the average of all of
     the numbers. */
     // Your code here
+let sum= num1+num2+num3+num4
+    return sum/4
 };
 
 function doubler(nums) {
     /* Takes an array of numbers and returns a new array where every element of
     the original array is multiplied by 2. */
     // Your code here
+let newarray=[];
+    for(i=0; i<nums.length; i++){
+        let answer= nums[i]*2;
+        newarray.push(answer);
+    }
+return newarray;
 };
 
 function combineArrays(arr1, arr2) {
@@ -26,6 +38,7 @@ function combineArrays(arr1, arr2) {
     calling this method won't permanently change, also known as **mutate**,
     either array. */
     // Your code here
+return arr1.concat(arr2);
 };
 
 function wordWithinArray(word, arr) {
@@ -33,42 +46,78 @@ function wordWithinArray(word, arr) {
     boolean that returns true if that string is located inside of the array, or
     false if it does not. Use `Array.indexOf`. */
     // Your code here
+if(arr.indexOf(word) !==-1){
+    return true
+}
+
 };
 
 function echo(str) {
-    /* Takes in a string and returns that string "echo-ized". E.g. 
+    /* Takes in a string and returns that string "echo-ized". E.g.
     echo("Mom!"); // => returns "MOM! ... Mom! ... mom!"
     echo("hey"); // => returns "HEY ... hey ... hey"
     echo("JUMp"); // => returns "JUMP ... JUMp ... jump" */
     // Your code here
+
+let up=str.toUpperCase();
+    let down=str.toLowerCase();
+    return up + " ... " + str + " ... " + down;
+
 };
 
 function fizzBuzz(max) {
     /* Takes a number, max and returns an array that contains every number from
     0 to max (not inclusive) that is divisible by either 3 or 5, **but not both**. */
     // Your code here
+    let newarray=[];
+    for(i=0; i<max ; i++){
+        if(i%3==0 && i%5!=0){
+            newarray.push(i);
+}
+            else if (i%3!=0 && i%5==0){
+             newarray.push(i);
+            }
+
+    };
+return newarray;
 };
 
 function hello(name) {
     /* Takes in a string name and returns a string saying "Hello, " to that name. */
     // Your code here
+return "Hello, " + name;
+
 };
 
 function goodbye(name) {
     /* Takes in a string name and returns a string saying "Bye, " to that name. */
     // Your code here
+return "Bye, " + name;
 };
 
 function isFive(num) {
     /* Takes in a number, num, and returns `true` if a number is equal to 5 and
     `false` if it is not. */
     // Your code here
+    if(num==5){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 function isOdd(num) {
     /* Takes in a number and returns `true` if the number is odd and returns
     `false` otherwise. Try writing this with and without `if` statements */
     // Your code here
+    if(num%2 !=0){
+        return true;
+    }
+    else{
+      return  false;
+    }
+
 };
 
 function isSubString(searchString, subString) {
@@ -76,6 +125,14 @@ function isSubString(searchString, subString) {
     `true` if `subString` is a part of the`searchString`, regardless of upper
     or lower case, and `false` if otherwise. */
     // Your code here
+let newsearch=searchString.toLowerCase();
+    let newsub=subString.toLowerCase();
+    if(newsearch.indexOf(newsub) !=-1){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 function aCounter(word) {
@@ -84,12 +141,12 @@ function aCounter(word) {
     function to use a `for` loop instead of the `while` loop it is currently
     using. */
 
-    /* 
+    /*
     let index = 0;
     let count = 0;
     while (index < word.length) {
         let char = word[index];
-        if (char === "a" || char === "A") {
+        if (char == "a" || char == "A") {
         count += 1;
         }
         index++;
@@ -97,8 +154,18 @@ function aCounter(word) {
     return count;
     */
     // Your code here
-};
+let count=0;
+    for(index=0; index<word.length;index++){
+let char =word[index];
 
+        if (char == "a" || char == "A") {
+
+           count += 1;
+        }
+
+    };
+return count;
+};
 module.exports = {
     divideByThree,
     averageOfTwo,
@@ -113,5 +180,5 @@ module.exports = {
     isFive,
     isOdd,
     isSubString,
-    aCounter   
+    aCounter
 }
